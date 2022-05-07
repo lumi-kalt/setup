7037520adf60660493df5fe7de8fe2ce51dc00c2
lumi-kalt <lumi.mia.kalt@gmail.com>
2022-05-07 18:05:22 +0100

    Add dry-comparisons

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <fmt/core.h>
+#include "dry-comparisons.hpp"
+
+using namespace rollbear;
 
 int main() {
-    fmt::print("Hello World!");
+	fmt::print("{}", 
+				any_of(std::plus{}, std::minus{})(2, 3) == 5);
 }
