6bfd6caa8d9f96155c57d35a2860dd185dcc4bf9
lumi-kalt <lumi.mia.kalt@gmail.com>
2022-05-08 00:31:17 +0100

    Write actual example code.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
+#include <string>
+#include <string_view>
+#include <vector>
+
 #include <fmt/core.h>
+#include <scn/scn.h>
+
 #include "dry-comparisons.hpp"
 #include "tl/optional.hpp"
 
 using namespace rollbear;
 
-int main() {
-	fmt::print("{}\n", 
-				any_of(std::plus{}, std::minus{})(2, 3) == 5);
+class Kitten {
+	std::string name;
+public:
+	Kitten(std::string_view name) : name{name} {}
+
+	static auto pat(Kitten k) noexcept
+	-> tl::optional<Kitten>  {
+		fmt::print("*patpat {}*\n", k.name);
+		return k;
+	}
+
+	static auto feed(Kitten k) noexcept
+	-> tl::optional<Kitten> {
+		fmt::print("{} is fed a nice bowl of food.\n", k.name);
+		return k;
+	}
+};
+
+auto main()
+-> int {
+	std::string name;
+	auto _ = scn::prompt("Kitty name? ", "{}", name);
+	tl::optional<Kitten> kitty;
+	kitty = Kitten{name};
+	kitty.and_then(Kitten::feed)
+		 .map(Kitten::pat);
+
+	kitty = tl::nullopt;
+	kitty.and_then(Kitten::feed)
+		 .map(Kitten::pat)
+		 .or_else([] { fmt::print("\nThis is not a kitten!?"); });
+
 }
